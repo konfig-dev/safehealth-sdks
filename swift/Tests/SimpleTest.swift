f08e2df5c8f57efc088ee4057c9932e60a2f539b
dphuang2 <dylan@konfigthis.com>
2024-02-13 11:29:37 -0800

    add userDevice.create test

--- swift/Tests/SimpleTest.swift
+++ swift/Tests/SimpleTest.swift
@@ -17,6 +17,47 @@ class SimpleTest: XCTestCase {
 
     override func tearDownWithError() throws {}
 
+    func testUserDeviceCreate() async throws {
+        let id = "id_example"
+        let createdAt = Date()
+        let updatedAt = Date()
+        let deviceId = "deviceId_example"
+        let appVersion = "appVersion_example"
+        let deviceModel = "deviceModel_example"
+        let deviceToken = "deviceToken_example"
+        let deviceType = "deviceType_example"
+        let idfaId = "idfaId_example"
+        let isLoggedIn = true
+        let osVersion = "osVersion_example"
+        let voipToken = "voipToken_example"
+        let userId = "userId_example"
+        let tenantId = "tenantId_example"
+        let userUUID = "userUUID_example"
+        let nationalHealthId = "nationalHealthId_example"
+        let language = "language_example"
+        let createResponse = try await self.safehealth.userDevice.create(
+            id: id,
+            createdAt: createdAt,
+            updatedAt: updatedAt,
+            deviceId: deviceId,
+            appVersion: appVersion,
+            deviceModel: deviceModel,
+            deviceToken: deviceToken,
+            deviceType: deviceType,
+            idfaId: idfaId,
+            isLoggedIn: isLoggedIn,
+            osVersion: osVersion,
+            voipToken: voipToken,
+            userId: userId,
+            tenantId: tenantId,
+            userUUID: userUUID,
+            nationalHealthId: nationalHealthId,
+            language: language
+        )
+        print(createResponse)
+        XCTAssertNotNil(createResponse)
+    }
+
     func testDeviceStore() async throws {
         let userId = UUID().uuidString
         let device = Device(
